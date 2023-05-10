4b2ac72e3373510ab2512aa2390962524ff3f508
kesmailzadeh <a1851935@adelaide.edu.au>
2023-05-10 13:45:52 +0000

    commit

---
+++ webpage.js
@@ -0,0 +1,11 @@
+const vueinst = new Vue ({
+    el: "#nav",
+    data: {
+        top_menu: [
+            { title:'Home', url:'#' },
+            { title:'About', url:'#' },
+            { title:'Clubs', url:'#' },
+            { title:'Login', url:'#' },
+        ],
+    }
+});
