c7b43d4df484d9099b906c86027d17dbaccb2962
xiaoyu42 <a1863278@adelaide.edu.au>
2023-06-05 13:43:57 +0000

    database actually added

---
+++ db/the_database.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE the_database;
+USE the_database;
+
+CREATE TABLE users (
+    user_id INT,
+    user_type VARCHAR(20),
+    user_name VARCHAR(30),
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    email VARCHAR(80),
+    password VARCHAR(30),
+    PRIMARY KEY (user_id)
+);
+CREATE TABLE clubs (
+    club_id INT,
+    club_name VARCHAR(30),
+    PRIMARY KEY (club_id)
+);
+
+CREATE TABLE events (
+    event_id INT,
+    event_name VARCHAR(30),
+    club_id INT,
+    event_description VARCHAR(1000),
+    event_date DATE,
+    event_time TIME,
+    PRIMARY KEY (event_id),
+    FOREIGN KEY (club_id) REFERENCES clubs(club_id) ON DELETE CASCADE
+);
+CREATE TABLE updates (
+    update_id INT,
+    club_id INT,
+    update_title VARCHAR(30),
+    update_description VARCHAR(1000),
+    PRIMARY KEY (update_id),
+    FOREIGN KEY (club_id) REFERENCES clubs(club_id) ON DELETE CASCADE
+);
+CREATE TABLE club_managers (
+    user_id INT,
+    club_id INT,
+    PRIMARY KEY (user_id,club_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (club_id) REFERENCES clubs(club_id) ON DELETE CASCADE
+);
+CREATE TABLE event_enrolments (
+    user_id INT,
+    event_id INT,
+    PRIMARY KEY (user_id,event_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events(event_id) ON DELETE CASCADE
+);
+CREATE TABLE club_enrolments (
+    user_id INT,
+    club_id INT,
+    PRIMARY KEY (user_id,club_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (club_id) REFERENCES clubs(club_id) ON DELETE CASCADE
+);
