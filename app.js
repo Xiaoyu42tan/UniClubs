82cdf4ef4a623df95c7a8b94ace587edf3dc1577
Dimos Karagiannis <dtkaragiannis@gmail.com>
2023-06-05 23:09:44 +0000

    Added login feature

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -6,28 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-// use mysql
-var mysql = require('mysql');
-
-// create a 'pool' (a group) of connections to be used for connecting with our SQL server
-var dbConnectionPool = mysql.createPool({
-    host: 'localhost',
-    database: 'the_database'
-});
-
 var app = express();
 
-// middleware for accessing database,
-// need access to the database before routes are processed in index.js
-
-// express will run this function on every request and the continue to the next module
-// (which is index.js)
-
-// for all requests handled in index.js, we can access the connection pool with req.pool
-app.use(function(req,res,next) {
-    req.pool = dbConnectionPool;
-    next();
-});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -37,5 +21,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,86 @@
 var express = require('express');
 var router = express.Router();
 
+const CLIENT_ID = '521244062266-prh5adjbtjmgk7vga4g1t8j515u0kt9l.apps.googleusercontent.com';
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* store users page. */
+  let users = {
+    user1: {password: 'password'},
+    user2: {password: 'password2'}
+  };
+
+//login
+router.post('/login', async function(req, res, next){
+  if('client_id' in req.body){
+    const ticket = await client.verifyIdToken({
+        idToken: req.body.credential,
+        audience: CLIENT_ID,// Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    console.log(payload['email']);
+    res.send();
+    // If request specified a G Suite domain:
+    // const domain = payload['hd'];
+  }else if(req.body.username in users && req.body.password === users[req.body.username].password){
+    req.session.username = req.body.username;
+    res.end();
+  }else{
+    res.sendStatus(401);
+  }
+  req.session.username = req.body.username;
+  console.log(JSON.stringify(req.body));
+  res.end();
+});
+
+//signup
+router.post('/signup', function(req, res, next){
+  if(req.body.username in users){
+    res.sendStatus(401);
+  }else{
+    req.session.username = req.body.username;
+    users[req.body.username] = {password: req.body.password};
+    res.end();
+  }
+  req.session.username = req.body.username;
+  console.log(JSON.stringify(req.body));
+  res.end();
+});
+
+//log out
+router.post('/logout', function(req, res, next){
+  if(username in req.session){
+    delete req.session.username;
+    res.end();
+  }else if(req.readyState == 4 && req.status == 403){
+    alert('Not logged in');
+  }
+});
+
+// This code handles a dedicated login route that would be made through a redirect
+router.post('/google_login', async function (req, res, next) {
+
+  const ticket = await client.verifyIdToken({
+      idToken: req.body.credential,
+      audience: CLIENT_ID
+  });
+  const payload = ticket.getPayload();
+  //console.log(payload['sub']);
+  console.log(payload['email']);
+  // If request specified a G Suite domain:
+  // const domain = payload['hd'];
+
+  // Redirect the user to the desired page after login. e.g. user's profile page.
+  res.redirect('/index.html');
+
 });
 
 module.exports = router;
