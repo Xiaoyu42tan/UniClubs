7deeca5fbd95d290e07a180c8aa56ab48f18baa1
xiaoyu42 <a1863278@adelaide.edu.au>
2023-06-09 13:41:44 +0000

    final final commit

--- db/the_database_backup.sql
+++ db/the_database_backup.sql
@@ -74,7 +74,7 @@ CREATE TABLE `club_managers` (
 
 LOCK TABLES `club_managers` WRITE;
 /*!40000 ALTER TABLE `club_managers` DISABLE KEYS */;
-INSERT INTO `club_managers` VALUES (5,1),(6,2);
+INSERT INTO `club_managers` VALUES (2,1);
 /*!40000 ALTER TABLE `club_managers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -87,10 +87,12 @@ DROP TABLE IF EXISTS `clubs`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `clubs` (
   `club_id` int NOT NULL AUTO_INCREMENT,
+  `club_url` varchar(30) DEFAULT NULL,
   `club_name` varchar(30) DEFAULT NULL,
   `club_description` varchar(1000) DEFAULT NULL,
-  PRIMARY KEY (`club_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`club_id`),
+  UNIQUE KEY `club_url` (`club_url`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,7 +101,7 @@ CREATE TABLE `clubs` (
 
 LOCK TABLES `clubs` WRITE;
 /*!40000 ALTER TABLE `clubs` DISABLE KEYS */;
-INSERT INTO `clubs` VALUES (1,'Archery','This is the Archery Club'),(2,'Computer Science','This is the Computer Science Club');
+INSERT INTO `clubs` VALUES (1,'club1.html','Archery Club','The Archery Club is for all those passionate about the old bow and arrow!\n    We accept those of all skill levels, regardless of culture or background!\n    Register to join our club so you can RSVP to events!'),(2,'club2.html','Computer Science Club','A club dedicated to Computer Science!\n    We talk all things comp sci, we do workshops, competitive programming, games nights and more!\n    You dont even have to be a comp sci student to join!\n    Press register to club to be able to RSVP for events!'),(3,'club3.html','Tech Innovators Club','Explore technologys frontiers and collaborate on coding, robotics, AI, and VR projects. Unleash your inner innovator and shape the future of tech!'),(4,'club4.html','Outdoor Adventure Society','Embrace natures beauty with hiking, camping, and rock climbing. Join fellow adventurers for thrilling outdoor experiences and unforgettable memories!'),(5,'club5.html','Artistic Expressions Society','Celebrate art in all its forms! Join artists of all levels for workshops, exhibitions, open mic nights, and collaborative projects. Unleash your imagination!'),(6,'club6.html','Sustainable Living Coalition','Promote eco-consciousness through educational initiatives, workshops, and projects. Be a steward of the planet and contribute to a greener future!'),(7,'club7.html','Language Club','Embrace diversity, learn languages, and immerse in different cultures. Join us for language workshops, cultural events, and international film screenings.'),(8,'club8.html','Health and Wellness Society','Prioritize holistic well-being through fitness challenges, mindfulness sessions, nutrition workshops, and mental health awareness campaigns.'),(9,'club9.html','Music Appreciation Club','Celebrate the power of music! Jam sessions, open mics, and music trivia nights. Join us to discover new sounds and connect through melodies!'),(10,'club10.html','Social Impact Collective','Drive positive change! Volunteer, fundraise, and advocate for social issues. Empower marginalized communities and make a lasting impact.');
 /*!40000 ALTER TABLE `clubs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -200,11 +202,11 @@ CREATE TABLE `users` (
   `first_name` varchar(30) DEFAULT NULL,
   `last_name` varchar(30) DEFAULT NULL,
   `email` varchar(80) DEFAULT NULL,
-  `password` varchar(30) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
   `phone_number` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_name` (`user_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -213,7 +215,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'user','user1','first_name1','last_name1','user1@gmail.com','password1','1111111111'),(2,'user','user2','first_name2','last_name2','user2@gmail.com','password2','2222222222'),(3,'user','user3','first_name3','last_name3','user3@gmail.com','password3','3333333333'),(4,'user','user4','first_name4','last_name4','user4@gmail.com','password4','4444444444'),(5,'manager','archery_manager','first_name5','last_name5','archery_manager@gmail.com','password5','5555555555'),(6,'manager','compsci_manager','first_name6','last_name6','compsci_manager@gmail.com','password6','6666666666');
+INSERT INTO `users` VALUES (1,'user','Bob','Bob','Foo','bobby@outlook.com','$argon2id$v=19$m=65536,t=3,p=4$dY3Pr3MWtlGAOfvUPrIKlQ$ACLQNf66uQm0eG4YJrBKTHcqdfKSQrLQ+GaywIhN2is','04123456789'),(2,'user','Archer','Archie','Bowie','archer@gmail.com','$argon2id$v=19$m=65536,t=3,p=4$pf192aTpW41fiLyWnrqGzg$2aCwH9JRvEbPHtGcJxc4/r41qJ2/cMPCCV5WloiT1ps','04987654321'),(3,'admin','Sys','Sysi','admino','systemad@hotmail.com','$argon2id$v=19$m=65536,t=3,p=4$D5yog416Q0ij0sh9DWgeCA$3G+yjrZChWNvDCJbzUP1+C3MfX3djDoyI9yxDLIHPSw','04987654321');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -226,4 +228,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-09  3:42:41
+-- Dump completed on 2023-06-09 13:41:10
